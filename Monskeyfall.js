a00fa3cc2dcf6e61e41512941bb32f2ae6f622db
wonk93 <danielsalasrodriguez93@gmail.com>
2023-03-13 19:59:14 +0100

    empezado

---
+++ Monskeyfall.js
@@ -0,0 +1,47 @@
+class Monkeyfall {
+    constructor(ctx, gameW, gameH, keys) {
+      this.ctx = ctx;
+      this.gameWidth = gameW;
+      this.gameHeight = gameH;
+  
+      this.width = 50;
+      this.height = 100;
+  
+      this.image = new Image();
+      this.image.src = "./images/car.png";
+  
+      this.posX = 250 - 76 / 2;
+      this.posY = this.gameHeight - this.height - 20;
+  
+      this.keys = keys;
+  
+      this.setListeners();
+    }
+  
+    draw() {
+      this.ctx.drawImage(
+        this.image,
+        this.posX,
+        this.posY,
+        this.width,
+        this.height
+      );
+    }
+  
+    setListeners() {
+      document.addEventListener("keydown", (e) => {
+        switch (e.keyCode) {
+          case this.keys.LEFT:
+            if (this.posX >= 80) {
+              this.posX -= 20;
+            }
+            break;
+          case this.keys.RIGHT:
+            if (this.posX + this.width <= this.gameWidth - 80) {
+              this.posX += 20;
+            }
+            break;
+        }
+      });
+    }
+  }
